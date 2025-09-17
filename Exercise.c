8d39dbd814e910eac93ca237e045bbd44ed0eb61
Synthia Tithi <synthiatithi02@gmail.com>
2025-09-18 01:21:57 +0600

    Add files via upload

---
+++ Exercise.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    char name []= "synthia";
+    int age= 20;
+    float height= 5.2;
+    char grade= 'A';
+        printf("name: %s, age: %d, height: %f, grade: %c\n", name, age, height, grade);
+return 0;
+}
